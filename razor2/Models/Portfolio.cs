0275265af067f36be620febd6c06bcea0a880e0f
richie <richie.kwon1@gmail.com>
2022-03-09 21:13:39 +0000

    Model

---
+++ razor2/Models/Portfolio.cs
@@ -0,0 +1,12 @@
+namespace razor2.Models
+{
+    public class Portfolio
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+        public string Description { get; set; }
+        
+        
+    }
+}
