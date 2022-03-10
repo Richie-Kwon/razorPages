4621ad791b5ba210aec85ada7b583a668dbbf3f3
richie <richie.kwon1@gmail.com>
2022-03-10 22:22:23 +0000

    iterable

--- razor2/Models/Portfolio.cs
+++ razor2/Models/Portfolio.cs
@@ -1,12 +1,20 @@
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
 namespace razor2.Models
 {
     public class Portfolio
     {
         public int Id { get; set; }
-
         public string Title { get; set; }
         public string Description { get; set; }
-        
-        
+       
+        [JsonPropertyName("img")]
+        public string Image { get; set; }
+
+        public override string ToString()
+        {
+            return JsonSerializer.Serialize<Portfolio>(this);
+        }
     }
 }
---
+++ razor2/Services/PortfolioServiceJsonFile.cs
@@ -0,0 +1,13 @@
+using System.Collections.Generic;
+using razor2.Models;
+
+namespace razor2.Services
+{
+    public class PortfolioServiceJsonFile
+    {
+        public IEnumerable<Portfolio> GetPortfolios()
+        {
+            return null;
+        }
+    }
+}
