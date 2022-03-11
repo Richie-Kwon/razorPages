73c417ee6294e35fbf4c4d2307eeba9b7dafc807
richie <richie.kwon1@gmail.com>
2022-03-11 16:15:30 +0000

    dotnet class of 31

---
+++ Jsontest/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.AspNetCore.Http.Features;
+using razor2.Services;
+    
+namespace Jsontest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var portfolios = new PortfolioServiceJsonFile().GetPortfolios();
+            foreach (var item in portfolios){
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
--- razor2/Services/PortfolioServiceJsonFile.cs
+++ razor2/Services/PortfolioServiceJsonFile.cs
@@ -1,13 +1,24 @@
 using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
 using razor2.Models;
 
 namespace razor2.Services
 {
     public class PortfolioServiceJsonFile
     {
-        public IEnumerable<Portfolio> GePortfolios()
+        public IEnumerable<Portfolio> GetPortfolios()
         {
-            return null;
+            var jsonFileName = @"/Users/richie/RiderProjects/razorPages/razor2/wwwroot/Portfolios/portfolios.json";
+
+            using (var jsonFileReader = File.OpenText(jsonFileName))
+            {
+                var options = new JsonSerializerOptions() { PropertyNameCaseInsensitive = true };
+                var AA=JsonSerializer.Deserialize<Portfolio[]>(jsonFileReader.ReadToEnd(),options);
+                return AA;
+            }
+
+            
         }
     }
 }
