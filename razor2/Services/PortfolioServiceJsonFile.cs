e44d18760d26a7bda4a156597910ab6a3b15e7c6
richie <richie.kwon1@gmail.com>
2022-03-12 12:13:24 +0000

    interface

--- razor2/Services/PortfolioServiceJsonFile.cs
+++ razor2/Services/PortfolioServiceJsonFile.cs
@@ -1,12 +1,20 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Text.Json;
+using Microsoft.AspNetCore.Hosting;
 using razor2.Models;
 
 namespace razor2.Services
 {
     public class PortfolioServiceJsonFile
     {
+        private readonly IWebHostEnvironment _webHostEnvironment;
+        
+        public PortfolioServiceJsonFile(IWebHostEnvironment webHostEnvironment)
+        {
+            this._webHostEnvironment = webHostEnvironment;
+        }
+
         public IEnumerable<Portfolio> GetPortfolios()
         {
             var jsonFileName = @"/Users/richie/RiderProjects/razorPages/razor2/wwwroot/Portfolios/portfolios.json";
@@ -14,11 +22,9 @@ namespace razor2.Services
             using (var jsonFileReader = File.OpenText(jsonFileName))
             {
                 var options = new JsonSerializerOptions() { PropertyNameCaseInsensitive = true };
-                var AA=JsonSerializer.Deserialize<Portfolio[]>(jsonFileReader.ReadToEnd(),options);
+                var AA = JsonSerializer.Deserialize<Portfolio[]>(jsonFileReader.ReadToEnd(), options);
                 return AA;
             }
-
-            
         }
     }
 }
